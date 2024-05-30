1e7e357f660fcbb14339bc8d7f6e0fa88eaee33b
Diego Barboza <diegojose1994@gmail.com>
2024-05-30 17:32:03 -0400

    backup

--- src/views/HomeView.jsx
+++ src/views/HomeView.jsx
@@ -1,8 +1,11 @@
+import { useState } from "react";
 import CardWeather from "../components/CardWeather";
 import useFetchWeather from "../components/hooks/useFetchWeather";
 
 const HomeView = () => {
   const [query, setQuery, weather, loading, error] = useFetchWeather();
+
+  const [currentCity, setCurrentCity] = useState();
 
   // useEffect(() => {
   //   fetch(`http://api.weatherapi.com/v1/current.json?key=${APIKEY}&q=${query}`)
@@ -11,10 +14,15 @@ const HomeView = () => {
   // }, [APIKEY, query]);
 
   const handleOnChange = (e) => {
-    setQuery(e.target.value);
+    // setQuery(e.target.value);
+    setCurrentCity(e.target.value);
   };
 
-  
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    setQuery(currentCity);
+  };
+
   return (
     <div className="overflow-hidden relative h-[100vh]">
       <div className="clouds"></div>
@@ -22,30 +30,33 @@ const HomeView = () => {
         className="w-full h-[100vh] text-white flex justify-center items-center relative"
         // style={{ backgroundImage: 'url("/images/bg-clouds-4.jpg")' }}
       >
-        {/* <div className="bg-[rgba(255,255,255,.3)] p-10 rounded-md shadow-2xl shadow-white/20 hover:shadow-white/50"> */}
-        {/* <div className="bg-sky-500 p-10 rounded-md shadow-2xl shadow-white/20 hover:shadow-white/50"> */}
-        <div className="bg-[rgba(0,0,0,.4)] p-10 rounded-md shadow-2xl shadow-white/20 hover:shadow-white/50">
-          <h1 className="text-3xl font-bold text-center mb-3">Weather App</h1>
-          <h2 className="mb-4">Introduzca el nombre de la ciudad que desea consultar</h2>
+        {weather ? (
+          <CardWeather current={weather.current} location={weather.location} />
+        ) : (
+          <div className="bg-gray-800 p-10 rounded-md">
+            <h1 className="text-3xl font-bold text-center mb-3">Weather App</h1>
+            <h2 className="mb-4 text-center">Introduzca una ciudad</h2>
 
-          <input
-            id="city"
-            type="search"
-            onChange={handleOnChange}
-            defaultValue={query}
-            placeholder="Ingresa la ciudad"
-            className="w-full text-white border border-gray-200 bg-transparent mb-8 rounded-md p-3 outline-none placeholder:text-gray-300"
-          />
+            <form onSubmit={handleOnSubmit} className="grid grid-cols-[7fr,3fr] gap-4 rounded-md">
+              <input
+                id="city"
+                type="text"
+                onChange={handleOnChange}
+                // defaultValue={query}
+                placeholder="Ingresa la ciudad"
+                className="border border-gray-200 bg-transparent p-3 rounded-sm outline-none placeholder:text-gray-300"
+              />
+              <input
+                type="submit"
+                value="Consultar"
+                className="border border-gray-300 px-6 rounded-sm cursor-pointer mx-auto"
+              />
+            </form>
 
-          {loading && <p>Loading...</p>}
-          {error && <p>{error}</p>}
-          {weather && (
-            <CardWeather
-              current={weather.current}
-              location={weather.location}
-            />
-          )}
-        </div>
+            {loading && <p>Loading...</p>}
+            {error && <p>{error}</p>}
+          </div>
+        )}
       </div>
     </div>
   );
